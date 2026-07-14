783ce11328a4652dd384af0f2adc498175d0a388
Arnav1510 <arnav.kulkarni.1510@gmail.com>
2026-07-14 13:59:13 +0530

    Add files via upload

--- vote.c
+++ vote.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+
 int main() {
     char name[30];
     printf("Enter your name: ");
@@ -7,13 +8,13 @@ int main() {
     printf("Hello %s, lets see if you can vote!\n", name);
     int age;
     char citi[30];
-    char e[30] = "Yes";
+    char ans[] = "Yes";
     printf("Enter Your age: ");
     scanf("%d", &age);
     printf("Are you a citizen of this country: ");
     scanf("%s", citi);
     if (age >= 18) {
-        if (citi == e) {
+        if (strcmp(citi, ans) == 0) {
             printf("You are allowed to vote!");
         } else {
             printf("You are not a citizen of this country!");
